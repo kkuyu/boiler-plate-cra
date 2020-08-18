fcb73784d5f0159e3b8b7fe2f5327f1244d83d05
kkuyu <devils67@naver.com>
2020-08-19 00:37:36 +0900

    BodyParser & PostMan & 회원 가입 기능

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const express = require('express');
 const app = express();
 const port = 5000;
+const bodyParser = require('body-parser');
+
+const { User } = require("./models/User");
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose');
 mongoose.connect('mongodb+srv://kkuyu:<password>@boiler-plate.t5xss.mongodb.net/<dbname>?retryWrites=true&w=majority', {
@@ -14,6 +23,23 @@ mongoose.connect('mongodb+srv://kkuyu:<password>@boiler-plate.t5xss.mongodb.net/
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
+})
+
+app.post('/register', (req, res) => {
+    const user = new User(req.body);
+
+    user.save((error, userInfo) => {
+        if (error) {
+            return res.json({
+                success: false,
+                error
+            });
+        } else {
+            return res.status(200).json({
+                success: true
+            });
+        }
+    });
 })
 
 app.listen(port, () => {
