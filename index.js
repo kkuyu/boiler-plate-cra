8eff21e29bb663ba1f4cdb89f29652832b25c683
kkuyu <devils67@naver.com>
2020-08-18 23:49:05 +0900

    몽고 DB 연결

--- index.js
+++ index.js
@@ -2,6 +2,16 @@ const express = require('express');
 const app = express();
 const port = 5000;
 
+const mongoose = require('mongoose');
+mongoose.connect('mongodb+srv://kkuyu:<password>@boiler-plate.t5xss.mongodb.net/<dbname>?retryWrites=true&w=majority', {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify: false
+    })
+    .then(() => console.log("MongoDB Connected..."))
+    .catch(error => console.log(error));
+
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
