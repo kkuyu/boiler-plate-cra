4b976a449f64ef7aa718fffdc0605f7ff556836a
kkuyu <devils67@naver.com>
2020-08-23 21:43:34 +0900

    router 정리

--- client/src/App.js
+++ client/src/App.js
@@ -6,24 +6,18 @@ import {
   Link
 } from 'react-router-dom';
 
-import LandingPage from './components/views/LandingPage/LandingPage';
-import LoginPage from './components/views/LoginPage/LoginPage';
-import RegisterPage from './components/views/RegisterPage/RegisterPage';
+import LandingPage from './components/views/LandingPage';
+import LoginPage from './components/views/LoginPage';
+import RegisterPage from './components/views/RegisterPage';
 
 function App() {
   return (
     <Router>
       <div>
         <Switch>
-          <Route exact path='/'>
-            <LandingPage />
-          </Route>
-          <Route path='/login'>
-            <LoginPage />
-          </Route>
-          <Route path='/register'>
-            <RegisterPage />
-          </Route>
+          <Route exact path='/' component={LandingPage} />
+          <Route exact path='/login' component={LoginPage} />
+          <Route exact path='/register' component={RegisterPage} />
         </Switch>
       </div>
     </Router>
