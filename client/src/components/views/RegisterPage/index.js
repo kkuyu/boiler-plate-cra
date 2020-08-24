5a30cfd3f9388b52498df089cfa6397fbf81b0f4
kkuyu <devils67@naver.com>
2020-08-24 21:25:12 +0900

    sign in

--- client/src/components/views/RegisterPage/index.js
+++ client/src/components/views/RegisterPage/index.js
@@ -1,8 +1,78 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { registerUser } from '../../../_actions/user_actions';
 
-function RegisterPage(){
+function RegisterPage(props){
+    const dispatch = useDispatch();
+    const [Email, setEmail] = useState('');
+    const [Name, setName] = useState('');
+    const [Password, setPassword] = useState('');
+    const [ConfirmPassword, setConfirmPassword] = useState('');
+
+    const onEmailHandler = (event) => {
+        setEmail(event.currentTarget.value);
+    }
+
+    const onNameHandler = (event) => {
+        setName(event.currentTarget.value);
+    }
+
+    const onConfirmPasswordHandler = (event) => {
+        setConfirmPassword(event.currentTarget.value);
+    }
+
+    const onPasswordHandler = (event) => {
+        setPassword(event.currentTarget.value);
+    }
+
+    const onSubmitHandler = (event) => {
+        event.preventDefault();
+
+        if(Password !== ConfirmPassword){
+            return alert('Check your password and confirmation password.');
+        }
+
+        let body = {
+            email: Email,
+            name: Name,
+            password: Password,
+            confirmPassword: ConfirmPassword
+        }
+
+        dispatch(registerUser(body))
+            .then(response => {
+                if(response.payload.success){
+                    props.history.push('/login')
+                }else{
+                    alert("Failed to sign up.")
+                }
+            });
+    }
+
     return (
-        <div>RegisterPage</div>
+        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100%', height: '100vh' }}>
+            <form
+                style={{
+                display: 'flex',
+                flexDirection: 'column'
+            }}
+            autoComplete='false'
+            onSubmit={onSubmitHandler}>
+                <label>Email</label>
+                <input type='email' value={Email} onChange={onEmailHandler} />
+                
+                <label>Name</label>
+                <input type='text' value={Name} onChange={onNameHandler} />
+                
+                <label>Password</label>
+                <input type='password' value={Password} onChange={onPasswordHandler} />
+    
+                <label>Confirm Password</label>
+                <input type='password' value={ConfirmPassword} onChange={onConfirmPasswordHandler} />
+    
+                <button type='submit'>Sign in</button>
+            </form>
+        </div>
     )
 }
 
