598874d694b2c5346abd4f2bf186322ddef16bbc
kkuyu <devils67@naver.com>
2020-08-24 21:30:50 +0900

    logout

--- client/src/components/views/LandingPage/index.js
+++ client/src/components/views/LandingPage/index.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import axios from 'axios';
 
-function LandingPage(){
+function LandingPage(props){
+    const onClickHandler = () => {
+        axios.get('/api/users/logout')
+            .then(response => {
+                if(response.data.success){
+                    props.history.push('/login');
+                }else{
+                    alert('Logout failed.');
+                }
+            })
+    }
+
     return (
         <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100%', height: '100vh' }}>
             <h2>시작 페이지</h2>
+            <button onClick={onClickHandler}>Logout</button>
         </div>
     )
 }
